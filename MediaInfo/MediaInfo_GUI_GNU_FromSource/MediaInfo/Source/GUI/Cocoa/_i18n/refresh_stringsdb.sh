79f7f4c298b12e0504b3c5cabdb197451c126059
wachin <wachin.id@gmail.com>
2020-06-04 21:29:30 -0500

    update

--- MediaInfo/MediaInfo_GUI_GNU_FromSource/MediaInfo/Source/GUI/Cocoa/_i18n/refresh_stringsdb.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-perl _update_file_in_stringsdb.pl ../English.lproj/About.strings
-perl _update_file_in_stringsdb.pl ../English.lproj/MainMenu.strings
-perl _update_file_in_stringsdb.pl ../English.lproj/MyWindow.strings
-perl _update_file_in_stringsdb.pl ../English.lproj/Preferences.strings
-perl _update_file_in_stringsdb.pl ../English.lproj/Localizable.strings
-perl _update_file_in_stringsdb.pl ../English.lproj/ServicesMenu.strings
